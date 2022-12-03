ed851190fee766aea33ee2a7ad79b44a80e42373
Chenrui Xu <91103116+ChenruiXu999@users.noreply.github.com>
2022-12-03 23:26:46 +0800

    Update solidity2.sol

--- solidity2.sol
+++ solidity2.sol
@@ -1,1 +1,45 @@
-1
+pragma solidity ^0.4.19;
+//调用同目录下的文件
+import "./zombiefactory.sol";
+
+//和另外定义好的一个合约的交互，这个interface里面getKitty是原来那个合约已经定义的
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );//这些都是拷贝过来的
+}
+
+//从同目录文件下引用的合约
+contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+  
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];//永久记忆的storage
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);//多返回
+    // 并修改函数调用
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
+}
